20eb5c7c6cb7fdcc4066ae8ad2a9c918a1891904
aayusht200 <aayush.t200@gmail.com>
2025-11-11 18:36:32 +0530

    Test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import "./style.css";
+import "./style.css"
+
+document.addEventListener("click", (e) => {
+	const isDropdownHeader = e.target.matches("[data-dropdown-header]")
+	const dropdown = e.target.closest("[data-dropdown]")
+	const submenu = dropdown?.querySelector(".sub-menu-wrap")
+
+	if (!isDropdownHeader && !dropdown) {
+		document
+			.querySelectorAll("[data-dropdown] .sub-menu-wrap.visible")
+			.forEach((menu) => {
+				menu.classList.remove("visible")
+			})
+		return
+	}
+	if (isDropdownHeader && submenu) {
+		submenu.classList.toggle("visible")
+	}
+})
